a7509a73b7cb80826ec080d9e88f84d269a7c0bf
CobaltDream <mrcobalt@outlook.com>
2018-05-28 16:12:04 +0200

    first commit

--- script.ps1
+++ script.ps1
@@ -1,5 +1,8 @@
-﻿function test ($firstname)
-{
-    echo $firstname
+﻿[int] $nombre = read-host "Donne moi un chiffre"
+
+if ($nombre % 2 -eq 0) {
+        echo "Even"
+    } else { 
+        echo "Odd"
 }
-test("Tommy")
+
